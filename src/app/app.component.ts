80ca96c887ee11be39d3c5a60dba34126a88be25
harsh dalal <dalalharsh1974@gmail.com>
2023-12-04 15:02:22 +0530

    search function working properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,4 +94,28 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  public searchEmployees(key: string): void{
+    console.log(key);
+    const results:Employee[] = [];
+    // for (const employee of this.employees){
+    //   if (employee.name.toLowerCase().includes(key.toLowerCase()) || employee.email.toLowerCase().includes(
+    //     key.toLowerCase())){
+    //       results.push(employee);
+    //     }
+    // }
+    for(const employee of this.employees){
+      if(employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+      || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1){
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+
+    if(results.length === 0 || !key){
+      this.getEmployees();
+    }
+  }
 }
